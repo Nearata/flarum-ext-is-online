636119a695ca469f8907989f2b157b55fa61f89a
William Di Cicco <nearata@protonmail.com>
2020-08-08 18:59:29 +0200

    Fix duplicate element
    * Fixed a bug where, on route changes, two alert are spawned instead of one

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,26 +1,22 @@
 import app from 'flarum/app';
-import { extend } from 'flarum/extend';
-import Page from 'flarum/components/Page';
 const isOnline = require('is-online');
 
 app.initializers.add('nearata/flarum-ext-is-online', () => {
-    extend(Page.prototype, 'config', () => {
-        const isOnlineElement = document.createElement('div');
+    const isOnlineElement = document.createElement('div');
 
-        isOnlineElement.id = 'is-online-alert';
-        isOnlineElement.innerText = app.translator.trans('nearata-is-online.forum.offline_text');
+    isOnlineElement.id = 'is-online-alert';
+    isOnlineElement.innerText = app.translator.trans('nearata-is-online.forum.offline_text');
 
-        document.body.appendChild(isOnlineElement);
+    document.body.appendChild(isOnlineElement);
 
-        setInterval(() => {
-            (async () => {
-                const ok = await isOnline();
-                if(ok) {
-                    isOnlineElement.classList.remove('offline');
-                } else {
-                    isOnlineElement.classList.add('offline');
-                }
-            })();
-        }, 1000);
-    });
+    setInterval(() => {
+        (async () => {
+            const ok = await isOnline();
+            if(ok) {
+                isOnlineElement.classList.remove('offline');
+            } else {
+                isOnlineElement.classList.add('offline');
+            }
+        })();
+    }, 1000);
 });
